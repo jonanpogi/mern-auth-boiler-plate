626d84e0b071fe6f5c636f8057b226c2c95785a2
Jonan Bie <jvbie@globaltelehealth.com.ph>
2023-10-07 12:43:06 +0800

    refactor check auth middleware

--- src/utils/checkAuth.ts
+++ src/utils/checkAuth.ts
@@ -1,8 +1,5 @@
 import {NextFunction, Request} from 'express';
-import {SessionData} from 'express-session';
 import jwt from 'jsonwebtoken';
-import {store} from '../models';
-import {IUsers} from '../models/Users';
 import {CustomResponse} from '../types';
 import constants from './constants';
 
@@ -12,38 +9,23 @@ const checkAuth = async (
   next: NextFunction,
 ): Promise<void | CustomResponse> => {
   try {
-    // Validate session id
-    if (!req.sessionID) {
-      throw new Error('Unauthenticated');
-    }
-
     // Check if the JWT token exists in the request header
     const token = req.headers.authorization?.replace('Bearer ', '');
 
-    if (!token) {
-      throw new Error('No token provided');
+    // Validate session id
+    if (!req.isAuthenticated() || !token) {
+      throw new Error('Unauthenticated');
     }
 
     // Decode the JWT token
-    const decodedToken = jwt.verify(token, constants.JWT_SECRET_KEY) as IUsers;
+    const decodedToken = jwt.verify(token, constants.JWT_SECRET_KEY);
 
     if (!decodedToken) {
       throw new Error('Invalid token');
     }
 
-    // Retrieve the user session from the database
-    const session = await new Promise((resolve, reject) => {
-      store.get(req.sessionID, (err, session) => {
-        if (err) reject(new Error('Error on fetching sessionn'));
-        else if (!session) reject(new Error('User session does not exist'));
-        resolve(session);
-      });
-    });
-
-    console.log('User session', session);
-
     // Verify expiry
-    if ((session as SessionData).cookie.expires === new Date()) {
+    if (req.session.cookie.expires!.getTime() < new Date().getTime()) {
       throw new Error('The user session already expires');
     }
 
